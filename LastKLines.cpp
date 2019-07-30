de42675175a1f60b128f6960124c56afca279db4
Brandie Young <brandieyoung98@gmail.com>
2019-07-30 14:01:14 -0400

    Update LastKLines.cpp

--- LastKLines.cpp
+++ LastKLines.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 using namespace std;
 
-//yields the index of the nth occurence of character c in the string s
+//helper method that yields the index in string s of the nth occurence of character c
 int findNthCharOccurence(int n, char c, string s){
     int count = 0;
     for(int i = 0; i< s.size(); i++){
@@ -15,31 +15,33 @@ int findNthCharOccurence(int n, char c, string s){
     }
     return -1; //error, said char was not found
 }
-
+//desired function that prints the last k lines based off user input
 void printLastKLines(int k){
 	string line = "";
 	string fileText = "";
 	int numLines = 0;
+	//reading in all input and counting the lines
 	while(!getline(cin, line).eof()){
 	    numLines++;
 	    fileText += line + '\n';
 	}
 	numLines++;
-	fileText += line; //get that last line
+	fileText += line; //account for that last line
 	
-	if(k == numLines){
+	if(k == numLines){ //this means the whole input
 	   cout << fileText << endl; 
 	}
-	else if(k <= 0 || k > numLines || fileText.empty()){
-	    cout << "Invalid input" << endl;
+	else if(k <= 0 || k > numLines || fileText.empty()){ //impossible k or no user input
+	    cout << "Invalid input" << endl; //error message
 	}
 	else{
-	    int lineBreak = numLines - k;
-	    string lastKLines = fileText.substr(findNthCharOccurence(lineBreak, '\n', fileText));
-	    cout << lastKLines << endl;
+	    int lineBreak = numLines - k; //the line number where we need to print from
+	    string lastKLines = fileText.substr(findNthCharOccurence(lineBreak, '\n', fileText)); //"break off" just the last k lines
+	    cout << lastKLines << endl; // and print em!
 	}
 }
-
+//testing in the main method
+//to test, change up the k value passed to the method and the user input of course
 int main(){
     printLastKLines(4);
     return 0;
